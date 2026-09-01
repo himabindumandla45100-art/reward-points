805b133c20165c878aff4c892b531bf5cbe66969
himabindumandla45100-art <himabindumandla45100@gmail.com>
2026-09-01 14:11:18 +0530

    Update RewardPointsApplication.java

--- reward-points/src/main/java/com/example/rewardpoints/RewardPointsApplication.java
+++ reward-points/src/main/java/com/example/rewardpoints/RewardPointsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RewardPointsApplication {
-/
     public static void main(String[] args) {
         SpringApplication.run(RewardPointsApplication.class, args);
     }
